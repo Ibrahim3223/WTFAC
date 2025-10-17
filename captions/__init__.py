44b0ec56b69575556b0651acff494ebd3d76499b
Ibrahim3223 <ibrahimyildirim3223@gmail.com>
2025-10-17 15:50:01 +0300

    Initialize captions module with imports

---
+++ captions/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Captions module."""
+
+from .karaoke_ass import build_karaoke_ass
+from .renderer import CaptionRenderer
+
+__all__ = ['build_karaoke_ass', 'CaptionRenderer']
